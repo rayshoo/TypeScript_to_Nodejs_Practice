685fcfd0732ceb9f37dc9d62516f111f881ce69a
dfnk5516 <rayshoo@naver.com>
2020-06-26 17:23:23 +0900

    express middleware

--- lecture/back/index.js
+++ lecture/back/index.js
@@ -1,9 +1,50 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var morgan = require("morgan");
+var cors = require("cors");
+var cookieParser = require("cookie-parser");
+var expressSession = require("express-session");
+var dotenv = require("dotenv");
+var passport = require("passport");
+var helmet = require("helmet");
+var hpp = require("hpp");
+dotenv.config();
 var app = express();
 var prod = process.env.NODE_ENV === 'production';
 app.set('port', prod ? process.env.PORT : 3065);
+if (prod) {
+    app.use(hpp());
+    app.use(helmet());
+    app.use(morgan('combined'));
+    app.use(cors({
+        origin: /nodebird\.com$/,
+        credentials: true
+    }));
+}
+else {
+    app.use(morgan('dev'));
+    app.use(cors({
+        origin: true,
+        credentials: true
+    }));
+}
+app.use('/', express.static('uploads'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        domain: prod ? '.nodebird.com' : undefined
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.get('/', function (req, res, next) {
     res.send('react nodebird 백엔드 정상 동작!');
 });
--- lecture/back/index.ts
+++ lecture/back/index.ts
@@ -1,10 +1,57 @@
 import * as express from 'express';
-import { Request, Response, NextFunction } from 'express';
+import * as morgan from 'morgan';
+import * as cors from 'cors';
+import * as cookieParser from 'cookie-parser';
+import * as expressSession from 'express-session';
+import * as dotenv from 'dotenv';
+import * as passport from 'passport';
+import * as helmet from 'helmet';
+import * as hpp from 'hpp';
+
+dotenv.config();
 
 const app: express.Application = express();
 const prod: boolean = process.env.NODE_ENV === 'production';
-
 app.set('port', prod ? process.env.PORT : 3065);
+
+if (prod) {
+  app.use(hpp());
+  app.use(helmet());
+  app.use(morgan('combined'));
+  app.use(
+    cors({
+      origin: /nodebird\.com$/,
+      credentials: true,
+    }),
+  );
+} else {
+  app.use(morgan('dev'));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    }),
+  );
+}
+
+app.use('/', express.static('uploads'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET!,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      domain: prod ? '.nodebird.com' : undefined,
+    },
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res, next) => {
   res.send('react nodebird 백엔드 정상 동작!');
